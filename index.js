16ebf2102e125b4894ca8708c0a9d2539a5503c0
ved1006 <dangeved106@gmail.com>
2024-12-24 13:12:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express  = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/check",(req,res) => {
+	let n = parseInt(req.query.number);
+	let r = n % 2 === 0? "even" : "odd";
+		res.json({"msg":r});
+		});
+
+app.listen(9000, () => { console.log("ready to serve @ 9000"); } );
